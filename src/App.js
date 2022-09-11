4ce6ed1e09c76a1282a7f2f72b90d12e6a1e4a4e
Aymeric Goube <aymericgoube@hotmail.fr>
2022-09-11 18:46:00 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import React, { useState } from "react";
+import myContact from "./contacts.json";
 
 function App() {
+  const fiveContacts = myContact.slice(0, 5);
+  const [stateApp, setStateApp] = useState(fiveContacts);
+  // console.log(myContact.length);
+  function addRandom() {
+    let random = 0;
+    do {
+      random = parseInt(Math.random() * myContact.length);
+    } while (stateApp.includes(myContact[random]));
+
+    setStateApp([...stateApp, myContact[random]]);
+  }
+  function sortByName() {
+    setStateApp([...stateApp].sort((a, b) => a.name.localeCompare(b.name)));
+  }
+  function sortByPopularity() {
+    setStateApp([...stateApp].sort((a, b) => a.popularity - b.popularity));
+  }
+  function deleteContact(id) {
+    let findIndex = stateApp.find((x) => {
+      return x.id === id.target.id;
+    });
+    const copy = [...stateApp];
+    copy.splice(stateApp.indexOf(findIndex), 1);
+    setStateApp(copy);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addRandom}>Add random contact</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Oscar Won</th>
+            <th>Emmy won</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stateApp.map((contact) => {
+            if (contact.wonOscar) {
+              contact.wonOscar = "🏆";
+            }
+            if (contact.wonEmmy) {
+              contact.wonEmmy = "🏆";
+            }
+            return (
+              <tr key={contact.id}>
+                <td className="contact-picture">
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td className="contact-name">{contact.name}</td>
+                <td className="contact-popularity">
+                  {contact.popularity.toFixed(2)}
+                </td>
+                <td>{contact.wonOscar}</td>
+                <td>{contact.wonEmmy}</td>
+                <button id={contact.id} onClick={deleteContact}>
+                  Delete
+                </button>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// const Table = (props) => {
+
+// const tableItems = props.contactInfo.map((ele) => {
+//   return <TableRow contactsData={ele} key={ele.id} onDeleteRow={deleteRow} />;
+//   return (
+//     <div>
+//       <table>
+//         <thead>
+//           <tr>
+//             <td>Picture</td>
+//             <td>Name</td>
+//             <td>Popularity</td>
+//             <td>wonOscar</td>
+//             <td>wonEmmy</td>
+//             <td>Actions</td>
+//           </tr>
+//         </thead>
+//         <tbody>{tableItems}</tbody>
+//       </table>
+//     </div>
+//   );
+// });
+
+// export default Table;
